825305548b3d738a8a798956284e65224ade955c
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-02-23 22:40:30 +0000

    Got some things refined, will get the speed powerups and jump done by this week, meanwhile will also start to get the door interaction done in

--- GPP Individual Work/Assets/PowerUp.cs
+++ GPP Individual Work/Assets/PowerUp.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a25c796c6b5f4179744a4193eb984f84c337342bdb561449c0e8196308cc3ec6
-size 726
+oid sha256:bf9e6d8459bd2c1c6211aa4b5341c93b8619bcf90e3f82c75938fad701db9b98
+size 1158
