268a8fd2841782c75fbf343144a633cbc1191484
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-03-01 20:56:34 +0000

    Got a simple door pressure plate logic, will refine the speed of the door opening and also will check with pressure plate will compress

---
+++ GPP Individual Work/Assets/DoorTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2351c4f0a5ae7dce60b7436c46267aacaec633cb2bca47d37602d9c8fb986ce
+size 418
--- GPP Individual Work/Assets/Player_Manager.cs
+++ GPP Individual Work/Assets/Player_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dca601537eaf83a0093bfb6efc1a49ce2b693178877dae0cfbf35a2f321fe716
-size 879
+oid sha256:5d4851e8cd2eaf2c24b6dd2cad3016cdb169853fff32beaca0e3561ab0fdcf27
+size 1050
