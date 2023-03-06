02a2596dca66061214a9ed9436225628c553e23f
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-03-06 11:55:37 +0000

    Got moving platform working, will see how to get spline working as well.

---
+++ GPP Individual Work/Assets/WaypointPath.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:760a30bfb05223cb2b78021facdc6838a3fc6a7f2e947e7c0352f37ca434aaf9
+size 544
---
+++ GPP Individual Work/Assets/movingPlatform.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78dd27cede7a4162e52692195b8fe044a2d67097b4506d7381efb63527c52f1e
+size 1872
