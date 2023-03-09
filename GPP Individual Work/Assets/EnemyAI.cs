dae87e95395308f9589a3beb213b046b1bdd79f6
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-03-09 19:06:54 +0000

    Got a Enemy AI working

---
+++ GPP Individual Work/Assets/EnemyAI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b69f556a664ec36e14a20256ca8f0e17c26d0b573ba9035e51b2f75672f4fb51
+size 1157
---
+++ GPP Individual Work/Assets/Follow_Player.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1c1cb140fe5e63025f8d8cebe9606ae8e650e6eedfd8de1233a39a7b8eeb2ab
+size 555
