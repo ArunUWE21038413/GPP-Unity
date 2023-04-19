1d7fb2e58db61a8771024db8dea0d0ee27eec42f
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-04-19 14:44:04 +0100

    Door Animation much smoother

--- GPP Individual Work/Assets/DoorTrigger.cs
+++ GPP Individual Work/Assets/DoorTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2351c4f0a5ae7dce60b7436c46267aacaec633cb2bca47d37602d9c8fb986ce
-size 418
+oid sha256:121ee21e18fc353296981248f1b3b9e4619b1351d3628bf452e55c123ac377ee
+size 496
