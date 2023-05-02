e7203a481b261e440a9ef0669879a96c072f9bb7
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-05-02 13:39:40 +0100

    still working on cam rotation on a path

--- GPP Individual Work/Assets/Rails.cs
+++ GPP Individual Work/Assets/Rails.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cfd2c6d3fabe747f543bd4e98625e3de95f5df07ca90b956b7183d6e5d6125e
-size 936
+oid sha256:6b1d16559f81879286f235c54fe07832072375a2edc416b1c2d3dae5121c9883
+size 963
