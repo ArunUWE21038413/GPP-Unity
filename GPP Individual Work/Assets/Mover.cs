4faa5764893f57f8e6abbdfb5540f3ad24123ca8
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-05-02 12:18:59 +0100

    Working on a camera moving along a path, tryna make sure i can move the player after the spline ends

---
+++ GPP Individual Work/Assets/Mover.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c816f558d964e238fbd5807fe01913e13ed3ea1e485cad8f07cf84c2d36f53a
+size 849
---
+++ GPP Individual Work/Assets/Rails.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7cfd2c6d3fabe747f543bd4e98625e3de95f5df07ca90b956b7183d6e5d6125e
+size 936
