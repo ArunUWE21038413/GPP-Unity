b69f9d133b9fc6fa7bb42b6fcdce90839595e121
Arun Hari <Arun2.Hariharasubramanian@live.uwe.ac.uk>
2023-05-03 18:33:47 +0100

    Got spline working

--- GPP Individual Work/Assets/Mover.cs
+++ GPP Individual Work/Assets/Mover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c816f558d964e238fbd5807fe01913e13ed3ea1e485cad8f07cf84c2d36f53a
-size 849
+oid sha256:c2865f02243b8baf59230bb0293732a13012405b309cdf09ad63b30a661e8f6f
+size 1290
--- GPP Individual Work/Assets/Rails.cs
+++ GPP Individual Work/Assets/Rails.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b1d16559f81879286f235c54fe07832072375a2edc416b1c2d3dae5121c9883
-size 963
+oid sha256:cf48460b0ab51d038697095309639e98fee55f2d08c8de93dbbecaf7ee955055
+size 962
